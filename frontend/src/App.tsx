e9348951541ef2130263c4e781d88dfa1da5401a
humble-race-condition <45735901+humble-race-condition@users.noreply.github.com>
2025-11-24 22:58:43 +0200

    Added basic project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = useState<number>(0);
+    const addition = () => setValue(prevValue => prevValue + 1);
+    return (
+        <div className="App">
+            <div>
+                Hello there
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'center', height: '500px' }}>
+                {value}
+            </div>
+            <button type="button" onClick={addition}>
+                Click me!
+            </button>
+        </div>
+    );
 }
 
 export default App;
