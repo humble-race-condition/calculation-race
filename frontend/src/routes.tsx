88d4b10118e4bfb885cd022f246b282ed1cd63f4
humble-race-condition <45735901+humble-race-condition@users.noreply.github.com>
2025-11-28 00:23:30 +0200

    Added basic routes

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,10 +1,11 @@
 import {createBrowserRouter} from "react-router";
 import App from "./App";
+import Navbar from "./navbar/Navbar";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <App />,
+        element: <Navbar />,
         children: [
 
         ]
