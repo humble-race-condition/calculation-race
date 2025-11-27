035045822e670a231893cb6af8f3abcb256b3d7e
humble-race-condition <45735901+humble-race-condition@users.noreply.github.com>
2025-11-28 00:02:36 +0200

    Adding routing

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import { store } from './store'
-import { Provider } from 'react-redux'
+import {store} from './store'
+import {Provider} from 'react-redux'
+import {RouterProvider} from "react-router";
+import router from "./routes";
+
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
-      <Provider store={store}>
-          <App />
-      </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <RouterProvider router={router}/>
+        </Provider>
+    </React.StrictMode>
 );
