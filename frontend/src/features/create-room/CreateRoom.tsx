6790b71ce645085ceed7e2a2a750c722c4a3a2a5
humble-race-condition <45735901+humble-race-condition@users.noreply.github.com>
2026-01-05 00:17:03 +0200

    Added create room

--- frontend/src/features/create-room/CreateRoom.tsx
+++ frontend/src/features/create-room/CreateRoom.tsx
@@ -1,24 +1,26 @@
+import styles from "./CreateRoom.module.css";
+
 function CreateRoom() {
     return (
         <div className="main">
             <h1 className="panel-title">Create a room</h1>
-            <div className="room-form-container">
-                <div className="room-name-container">
+            <div className={styles.roomFormContainer}>
+                <div className={styles.roomNameContainer}>
                     <label htmlFor="room-name" className="base-label">Name your room?</label>
                     <input type="text" id="room-name" placeholder="Enter room name" className="base-text-input"/>
                 </div>
-                <div className="room-type-container">
+                <div className={styles.roomTypeContainer}>
                     <p className="base-label">Type of room?
                         Select game type
                     </p>
                     <div>
-                        <input className="room-type-input" type="radio" id="red" name="color"/>
-                        <label className="room-type-label" htmlFor="red">Cooperative</label>
-                        <input className="room-type-input" type="radio" id="blue" name="color"/>
-                        <label className="room-type-label" htmlFor="blue">Competitive</label>
+                        <input className={styles.roomTypeInput} type="radio" id="red" name="color"/>
+                        <label className={styles.roomTypeLabel} htmlFor="red">Cooperative</label>
+                        <input className={styles.roomTypeInput} type="radio" id="blue" name="color"/>
+                        <label className={styles.roomTypeLabel} htmlFor="blue">Competitive</label>
                     </div>
                 </div>
-                <div className="room-button-container">
+                <div className={styles.roomButtonContainer}>
                     <button type="button" className="button">Create room</button>
                 </div>
             </div>
