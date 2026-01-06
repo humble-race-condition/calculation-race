2d7c80c5e1099cbdb7a3653f0499c36b2ff85db1
humble-race-condition <45735901+humble-race-condition@users.noreply.github.com>
2026-01-06 23:20:09 +0200

    Updated the module css to select the last selected nav link

--- frontend/src/shared/navbar/Navbar.tsx
+++ frontend/src/shared/navbar/Navbar.tsx
@@ -1,28 +1,45 @@
-import {NavLink} from "react-router";
 import {useDispatch, useSelector} from "react-redux";
 import {setDark} from "../../config/store/global";
 import {type RootState} from "../../config/store/storeConfiguration";
 import styles from "./Navbar.module.css";
+import {NavbarElement} from "./NavbarElement.tsx";
+
+type NavbarElement = {
+    id: number,
+    path: string,
+    label: string,
+}
 
 function Navbar() {
     const dispatch = useDispatch();
     const isDarkThemeChecked = useSelector((state: RootState) => state.global.isDarkThemeChecked);
+    const navbarData: NavbarElement[] = [
+        {
+            id: 1,
+            path: '/',
+            label: 'CalculationRace',
+        },
+        {
+            id: 2,
+            path: '/join-room',
+            label: 'Join Room',
+        },
+        {
+            id: 3,
+            path: '/create-room',
+            label: 'Create Room',
+        },
+        {
+            id: 4,
+            path: '/rankings',
+            label: 'Rankings',
+        },
+    ];
 
     return <>
         <nav className={styles.header}>
             <ul className={styles.headerList}>
-                <li className={styles.headerListElement}>
-                    <NavLink className={styles.headerListElementLink} to="/">CalculationRace</NavLink>
-                </li>
-                <li className={styles.headerListElement}>
-                    <NavLink className={styles.headerListElementLink} to="/join-room"> Join Room</NavLink>
-                </li>
-                <li className={styles.headerListElement}>
-                    <NavLink className={styles.headerListElementLink} to="/create-room">Create Room</NavLink>
-                </li>
-                <li className={styles.headerListElement}>
-                    <NavLink className={styles.headerListElementLink} to="/rankings">Rankings</NavLink>
-                </li>
+                {navbarData.map((item) => <NavbarElement {...item} key={item.id}/>)}
             </ul>
             <label className={styles.themeSwitch}>
                 <input className={styles.themeSwitchInput}
---
+++ frontend/src/shared/navbar/NavbarElement.tsx
@@ -0,0 +1,20 @@
+import styles from "./Navbar.module.css";
+import {NavLink} from "react-router";
+
+type NavbarElementProps = {
+    path: string,
+    label: string
+}
+
+export function NavbarElement({path, label}: NavbarElementProps) {
+    return (
+        <li className={styles.headerListElement}>
+            <NavLink
+                className={({isActive}) => isActive
+                    ? `${styles.headerListElementLink} ${styles.active}`
+                    : styles.headerListElementLink}
+                to={path}>
+                {label}
+            </NavLink>
+        </li>);
+}
