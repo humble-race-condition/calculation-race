8af09477af55c6ff55fe13e7b8086f665d562fd9
humble-race-condition <45735901+humble-race-condition@users.noreply.github.com>
2026-01-08 00:15:43 +0200

    Fixed scoreboard

--- frontend/src/features/game/Scoreboard.tsx
+++ frontend/src/features/game/Scoreboard.tsx
@@ -1,7 +1,282 @@
 import styles from "./Game.module.css";
 import PanelTitle from "../../shared/panel-title/PanelTitle.tsx";
+import ScoreboardRow from "./ScoreboardRow.tsx";
+
+interface ScoreboardRowValue {
+    id: string;
+    position: number;
+    username: string;
+    score: number;
+}
 
 export default function Scoreboard() {
+    const values: ScoreboardRowValue[] = [
+        {
+            id: "1",
+            position: 1,
+            username: "Gosho",
+            score: 15
+        },
+        {
+            id: "2",
+            position: 2,
+            username: "Customize Toolbar…",
+            score: 1
+        },
+        {
+            id: "3",
+            position: 321,
+            username: "Gosho123",
+            score: 15
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        },
+        {
+            id: "4",
+            position: 4,
+            username: "Gosho",
+            score: 125
+        }
+    ];
+
     return (
         <div className={styles.scoreboard}>
             <PanelTitle title={"Scoreboard"}/>
@@ -20,111 +295,7 @@ export default function Scoreboard() {
                 <div className={`${styles.scoreboardBodyTableContainer} ${styles.scrollbarContainer}`}>
                     <table className={styles.scoreboardTable}>
                         <tbody>
-                        <tr className={`${styles.scoreboardRow} ${styles.scoreboardRowBorder}`}>
-                            <td className={styles.scoreboardTablePositionColumn}>1</td>
-                            <td className={styles.scoreboardTableUsernameColumn}>Gosho</td>
-                            <td className={styles.scoreboardTableScoreColumn}>15</td>
-                        </tr>
-                        <tr className={`${styles.scoreboardRow} ${styles.scoreboardRowBorder}`}>
-                            <td className={styles.scoreboardTablePositionColumn}>2</td>
-                            <td className={styles.scoreboardTableUsernameColumn}>ToshoTodorov</td>
-                            <td className={styles.scoreboardTableScoreColumn}>1</td>
-                        </tr>
-                        <tr className={`${styles.scoreboardRow} ${styles.scoreboardRowBorder}`}>
-                            <td className={styles.scoreboardTablePositionColumn}>321</td>
-                            <td className={styles.scoreboardTableUsernameColumn}>Gosho123</td>
-                            <td className={styles.scoreboardTableScoreColumn}>15</td>
-                        </tr>
-                        <tr className={`${styles.scoreboardRow} ${styles.scoreboardRowBorder}`}>
-                            <td className={styles.scoreboardTablePositionColumn}>4</td>
-                            <td className={styles.scoreboardTableUsernameColumn}>Gosho</td>
-                            <td className={styles.scoreboardTableScoreColumn}>125</td>
-                        </tr>
-                        <tr className={`${styles.scoreboardRow} ${styles.scoreboardRowBorder}`}>
-                            <td className={styles.scoreboardTablePositionColumn}>4</td>
-                            <td className={styles.scoreboardTableUsernameColumn}>Gosho</td>
-                            <td className={styles.scoreboardTableScoreColumn}>125</td>
-                        </tr>
-                        <tr className={`${styles.scoreboardRow} ${styles.scoreboardRowBorder}`}>
-                            <td className={styles.scoreboardTablePositionColumn}>4</td>
-                            <td className={styles.scoreboardTableUsernameColumn}>Gosho</td>
-                            <td className={styles.scoreboardTableScoreColumn}>125</td>
-                        </tr>
-                        <tr className={`${styles.scoreboardRow} ${styles.scoreboardRowBorder}`}>
-                            <td className={styles.scoreboardTablePositionColumn}>4</td>
-                            <td className={styles.scoreboardTableUsernameColumn}>Gosho</td>
-                            <td className={styles.scoreboardTableScoreColumn}>125</td>
-                        </tr>
-                        <tr className={`${styles.scoreboardRow} ${styles.scoreboardRowBorder}`}>
-                            <td className={styles.scoreboardTablePositionColumn}>4</td>
-                            <td className={styles.scoreboardTableUsernameColumn}>Gosho</td>
-                            <td className={styles.scoreboardTableScoreColumn}>125</td>
-                        </tr>
-                        <tr className={`${styles.scoreboardRow} ${styles.scoreboardRowBorder}`}>
-                            <td className={styles.scoreboardTablePositionColumn}>4</td>
-                            <td className={styles.scoreboardTableUsernameColumn}>Gosho</td>
-                            <td className={styles.scoreboardTableScoreColumn}>125</td>
-                        </tr>
-                        <tr className={`${styles.scoreboardRow} ${styles.scoreboardRowBorder}`}>
-                            <td className={styles.scoreboardTablePositionColumn}>4</td>
-                            <td className={styles.scoreboardTableUsernameColumn}>Gosho</td>
-                            <td className={styles.scoreboardTableScoreColumn}>125</td>
-                        </tr>
-                        <tr className={`${styles.scoreboardRow} ${styles.scoreboardRowBorder}`}>
-                            <td className={styles.scoreboardTablePositionColumn}>4</td>
-                            <td className={styles.scoreboardTableUsernameColumn}>Gosho</td>
-                            <td className={styles.scoreboardTableScoreColumn}>125</td>
-                        </tr>
-                        <tr className={`${styles.scoreboardRow} ${styles.scoreboardRowBorder}`}>
-                            <td className={styles.scoreboardTablePositionColumn}>4</td>
-                            <td className={styles.scoreboardTableUsernameColumn}>Gosho</td>
-                            <td className={styles.scoreboardTableScoreColumn}>125</td>
-                        </tr>
-                        <tr className={`${styles.scoreboardRow} ${styles.scoreboardRowBorder}`}>
-                            <td className={styles.scoreboardTablePositionColumn}>4</td>
-                            <td className={styles.scoreboardTableUsernameColumn}>Gosho</td>
-                            <td className={styles.scoreboardTableScoreColumn}>125</td>
-                        </tr>
-                        <tr className={`${styles.scoreboardRow} ${styles.scoreboardRowBorder}`}>
-                            <td className={styles.scoreboardTablePositionColumn}>4</td>
-                            <td className={styles.scoreboardTableUsernameColumn}>Gosho</td>
-                            <td className={styles.scoreboardTableScoreColumn}>125</td>
-                        </tr>
-                        <tr className={`${styles.scoreboardRow} ${styles.scoreboardRowBorder}`}>
-                            <td className={styles.scoreboardTablePositionColumn}>4</td>
-                            <td className={styles.scoreboardTableUsernameColumn}>Gosho</td>
-                            <td className={styles.scoreboardTableScoreColumn}>125</td>
-                        </tr>
-                        <tr className={`${styles.scoreboardRow} ${styles.scoreboardRowBorder}`}>
-                            <td className={styles.scoreboardTablePositionColumn}>4</td>
-                            <td className={styles.scoreboardTableUsernameColumn}>Gosho</td>
-                            <td className={styles.scoreboardTableScoreColumn}>125</td>
-                        </tr>
-                        <tr className={`${styles.scoreboardRow} ${styles.scoreboardRowBorder}`}>
-                            <td className={styles.scoreboardTablePositionColumn}>4</td>
-                            <td className={styles.scoreboardTableUsernameColumn}>Gosho</td>
-                            <td className={styles.scoreboardTableScoreColumn}>125</td>
-                        </tr>
-                        <tr className={`${styles.scoreboardRow} ${styles.scoreboardRowBorder}`}>
-                            <td className={styles.scoreboardTablePositionColumn}>4</td>
-                            <td className={styles.scoreboardTableUsernameColumn}>Gosho</td>
-                            <td className={styles.scoreboardTableScoreColumn}>125</td>
-                        </tr>
-                        <tr className={`${styles.scoreboardRow} ${styles.scoreboardRowBorder}`}>
-                            <td className={styles.scoreboardTablePositionColumn}>4</td>
-                            <td className={styles.scoreboardTableUsernameColumn}>Gosho</td>
-                            <td className={styles.scoreboardTableScoreColumn}>125</td>
-                        </tr>
-                        <tr className={`${styles.scoreboardRow} ${styles.scoreboardRowBorder}`}>
-                            <td className={styles.scoreboardTablePositionColumn}>4</td>
-                            <td className={styles.scoreboardTableUsernameColumn}>Gosho</td>
-                            <td className={styles.scoreboardTableScoreColumn}>125</td>
-                        </tr>
-                        <tr className={`${styles.scoreboardRow} ${styles.scoreboardRowBorder}`}>
-                            <td className={styles.scoreboardTablePositionColumn}>4</td>
-                            <td className={styles.scoreboardTableUsernameColumn}>Gosho</td>
-                            <td className={styles.scoreboardTableScoreColumn}>125</td>
-                        </tr>
+                        {values.map((value) => <ScoreboardRow {...value} id={value.id}/>)}
                         </tbody>
                     </table>
                 </div>
---
+++ frontend/src/features/game/ScoreboardRow.tsx
@@ -0,0 +1,18 @@
+import styles from "./Game.module.css";
+
+type ScoreboardRowProps = {
+    id: string;
+    position: number;
+    username: string;
+    score: number;
+};
+
+export default function ScoreboardRow({position, username, score}: ScoreboardRowProps) {
+    return (
+        <tr className={`${styles.scoreboardRow} ${styles.scoreboardRowBorder}`}>
+            <td className={styles.scoreboardTablePositionColumn}>{position}</td>
+            <td className={styles.scoreboardTableUsernameColumn}>{username}</td>
+            <td className={styles.scoreboardTableScoreColumn}>{score}</td>
+        </tr>
+    );
+}
