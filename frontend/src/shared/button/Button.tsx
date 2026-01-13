320fe8a2494452410e3033628c28b86667717b91
humble-race-condition <45735901+humble-race-condition@users.noreply.github.com>
2026-01-14 00:12:24 +0200

    Updated button component

--- frontend/src/shared/button/Button.tsx
+++ frontend/src/shared/button/Button.tsx
@@ -1,13 +1,15 @@
 import styles from "./Button.module.css";
+import React from "react";
 
 interface ButtonProps {
     text: string;
+    onClick: (e: React.MouseEvent) => void;
 }
 
-export default function Button({text}: ButtonProps) {
+export default function Button({text, onClick}: ButtonProps) {
     return (
         <div>
-            <button type="button" className={styles.button}>{text}</button>
+            <button type="button" className={styles.button} onClick={onClick}>{text}</button>
         </div>
     );
 }
