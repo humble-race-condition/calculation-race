f7b67558cb3a192e879876b9498ab4fa79901e1a
humble-race-condition <45735901+humble-race-condition@users.noreply.github.com>
2026-01-14 00:12:12 +0200

    Updated text input component

--- frontend/src/shared/text-input/TextInput.tsx
+++ frontend/src/shared/text-input/TextInput.tsx
@@ -1,12 +1,21 @@
 import styles from "./TextInput.module.css";
+import React from "react";
 
 interface TextInputProps {
     id: string;
     placeholder: string;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function TextInput({id, placeholder}: TextInputProps) {
+export default function TextInput({id, placeholder, value, onChange}: TextInputProps) {
     return (
-        <input type="text" placeholder={placeholder} id={id} className={styles.baseTextInput}/>
+        <input type="text"
+               placeholder={placeholder}
+               id={id}
+               className={styles.baseTextInput}
+               value={value}
+               onChange={onChange}
+        />
     );
 }
