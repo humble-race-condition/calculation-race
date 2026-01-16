34292e16d2ce0684067e9f7f76088685bef8a0c1
humble-race-condition <45735901+humble-race-condition@users.noreply.github.com>
2026-01-17 00:37:24 +0200

    Fixed react imports

--- frontend/src/shared/set-state-utilities/setStateUtility.ts
+++ frontend/src/shared/set-state-utilities/setStateUtility.ts
@@ -1,8 +1,9 @@
+import React from "react";
+
 export function createSetField<T>(
     setState: React.Dispatch<React.SetStateAction<T>>
 ) {
     return <K extends keyof T>(key: K, value: T[K]) => {
-        debugger;
         setState(prev => ({
             ...prev,
             [key]: value,
