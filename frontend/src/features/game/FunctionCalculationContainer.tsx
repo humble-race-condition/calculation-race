965db87d2d0eeff2484543c77fd9c8a1768301e1
humble-race-condition <45735901+humble-race-condition@users.noreply.github.com>
2026-01-27 00:00:33 +0200

    Updated design css

--- frontend/src/features/game/FunctionCalculationContainer.tsx
+++ frontend/src/features/game/FunctionCalculationContainer.tsx
@@ -7,8 +7,11 @@ export default function FunctionCalculationContainer() {
         <div className={styles.gameContainer}>
             <h2 className={styles.gameFormula}>This is the mighty formula</h2>
             <form className={styles.submitResultContainer}>
+                //ToDo add overriding style and a default style for text input and button
                 <TextInput id={"submit-result-input"} placeholder={"Enter your result here"}/>
-                <Button text={"Submit"}/>
+                <div className={styles.buttonContainer}>
+                    <Button text={"Submit"}/>
+                </div>
             </form>
         </div>
     );
