b31a5e1521b4ba920bb22156584f3fa63bc2fe4d
humble-race-condition <45735901+humble-race-condition@users.noreply.github.com>
2026-02-28 14:53:37 +0200

    Updated frontend

--- frontend/src/features/game/Game.tsx
+++ frontend/src/features/game/Game.tsx
@@ -66,6 +66,12 @@ export function Chat() {
                     <textarea className={styles.chatInput} placeholder="Want to collaborate?"></textarea>
                 </label>
             </div>
+            <div className={styles.chatInputContainer}>
+                <label className={styles.chatLabel} htmlFor="chat-input">
+                    <textarea id="chat-input" className={styles.chatInput} placeholder="Want to collaborate?"></textarea>
+                </label>
+                <button className={styles.chatButton}>Send</button>
+            </div>
         </div>
     );
 }
